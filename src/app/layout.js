7dcc13c42c70850dc5edb8dc90c39e2e33d2f47d
Mhando-Z <Mhandosz17@gmail.com>
2024-12-15 07:05:21 +0300

    Phase One

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="flex flex-col min-h-screen overflow-x-hidden">
+        {children}
+      </body>
     </html>
   );
 }
